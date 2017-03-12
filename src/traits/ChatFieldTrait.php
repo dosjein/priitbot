c50debf44f74650f9f23adf95ac8ecbf1c1ea188
John Dosje <vk@dosje.in>
2017-03-12 10:28:41 +0200

    added css , basic trait

---
+++ src/traits/ChatFieldTrait.php
@@ -0,0 +1,13 @@
+<?php namespace Dosje_in\priitbot;
+
+use \GuzzleHttp\Client;
+use \GuzzleHttp\Exception\BadResponseException;
+
+use Config;
+use Cache;
+use Carbon\Carbon;
+
+
+trait ChatFieldTrait {
+
+}
